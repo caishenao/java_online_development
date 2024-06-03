ab5efc8d52b55c3b539fa9c4523058ce7697d4b8
caishenao <3194165094@qq.com>
2024-06-03 23:15:47 +0800

    init:添加loki支持（Java并不支持）

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ gitpod在线开发Java项目
 ## TODO
 - [x] 创建gitpod项目
 - [x] 搭建基础的Java开发环境
-- [ ] web-comment 编写统一响应对象
+- [x] web-comment 编写统一响应对象
 - [ ] web-comment 编写统一异常处理
-- [ ] 集成swagger
+- [x] 集成swagger
 - [ ] auth 集成数据库
 - [ ] auth 集成redis
+- [ ] 集成loki日志系统
--- projects/auth/src/main/java/cn/cai/auth/controller/HealthController.java
+++ projects/auth/src/main/java/cn/cai/auth/controller/HealthController.java
@@ -1,13 +1,23 @@
 package cn.cai.auth.controller;
 
+import cn.cai.web.comment.response.ResponseData;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 健康检查
+ *
+ * @author caishenao
+ */
 @RestController
+@Tag(name = "健康检查", description = "健康检查")
 public class HealthController {
 
     @GetMapping("/health")
-    public String health() {
-        return "ok"; // 返回一个简单的字符串
+    @Operation(summary = "健康检查")
+    public ResponseData<Void> health() {
+        return ResponseData.success();
     }
 }
