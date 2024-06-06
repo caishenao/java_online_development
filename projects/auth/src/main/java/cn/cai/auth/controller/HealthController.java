560d5874bd3681a795bc93400fbc1d8e9bad2dda
caishenao <3194165094@qq.com>
2024-06-06 09:06:47 +0800

    init:Java 添加loki支持

--- projects/auth/src/main/java/cn/cai/auth/controller/HealthController.java
+++ projects/auth/src/main/java/cn/cai/auth/controller/HealthController.java
@@ -3,6 +3,7 @@ package cn.cai.auth.controller;
 import cn.cai.web.comment.response.ResponseData;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @Tag(name = "健康检查", description = "健康检查")
+@Slf4j
 public class HealthController {
 
     @GetMapping("/health")
     @Operation(summary = "健康检查")
     public ResponseData<Void> health() {
+        log.info("访问了健康检查接口");
+        log.error("这是错误信息");
+        log.warn("这是警告信息");
+        log.debug("这是调试信息");
         return ResponseData.success();
     }
 }
