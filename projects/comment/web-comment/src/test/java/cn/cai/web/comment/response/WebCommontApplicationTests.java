33ff1f4744cf1ce3311c94547421b448051ccf2e
caishenao <3194165094@qq.com>
2024-10-05 16:48:14 +0800

    q

---
+++ projects/comment/web-comment/src/test/java/cn/cai/web/comment/response/WebCommontApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.cai.web.comment.response;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCommontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
